e7e7dcc7e6db001c9fa916c5fc95a0e57de7ba1a
ngocnk <ngoc.nguyen@bluecoglobal.com>
2025-07-09 16:47:29 +0700

    Add Logic Transaction

---
+++ src/models/Config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <string>
+
+const std::string USER_DATA_DIR = "./data/users";
+const std::string WALLET_DATA_DIR = "./data/wallets";
+const std::string TRANSACTION_DATA_DIR = "./data/transactions";
+const std::string SYSTEM_WALLET_ID = "WALLET_SYS";
+
+#endif // CONFIG_H
