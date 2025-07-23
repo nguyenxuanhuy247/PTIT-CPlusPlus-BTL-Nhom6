0812f2af6bb4e87ec42317345a87f3a893f67c89
khoa01250125 <nguyenanhkhoa25011996@gmail.com>
2025-07-23 10:19:55 +0700

    Người dùng thông thường

---
+++ src/core/menu/user_menu.h
@@ -0,0 +1,9 @@
+#ifndef USER_MENU_H
+#define USER_MENU_H
+
+#include "../models/User.h"
+
+// Hàm hiển thị menu người dùng và xử lý các thao tác liên quan đến User
+void showUserMenu(User& currentUser);
+
+#endif // USER_MENU_H
