aaadee32201199397bdaadec36735d0cb3243333
khoa01250125 <nguyenanhkhoa25011996@gmail.com>
2025-07-23 10:12:35 +0700

    tải dữ liệu

---
+++ src/core/loadAllData/loadAllData.cpp
@@ -0,0 +1,18 @@
+
+#include "../models/User.h"
+#include "../menu/manager_menu.h"
+#include "../menu/user_menu.h"
+#include "../data/DataStore.h"
+
+void handleLoggedInUser(User& user) {
+    DataStore::loadAllData();
+    
+    if (user.getRole() == UserRole::Manager) {
+        showManagerMenu(user);
+    } else {
+        showUserMenu(user);
+    }
+
+    // Tu dong dang xuat sau khi xong menu
+    user.setRole(UserRole::Failed);
+}
